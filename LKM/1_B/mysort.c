4a2b77ebe30724f011a61eb6985f0a018e689797
Ranjith Kumar <ranjith.dakshana2015@gmail.com>
2019-09-07 17:29:47 +0530

    1_B ok

--- LKM/1_B/mysort.c
+++ LKM/1_B/mysort.c
@@ -7,6 +7,7 @@
 #include <linux/list.h>
 #include <linux/sort.h>
 #include <linux/string.h>
+#include <linux/mutex.h>
 #include <linux/slab.h>         // kmalloc()
 
 MODULE_LICENSE("GPL");
@@ -39,22 +40,23 @@ static struct info_node{
 };
 
 static struct info_node *head = NULL;;
+DEFINE_MUTEX(lock);
 
 #define RW_PERM 0777
 #define TYPE_INT 0xff
 #define TYPE_STRING 0xf0
 
-int cmp_int(const void *a, const void *b)
+static int cmp_int(const void *a, const void *b)
 {
     return *(int *)a - *(int *)b;
 }
 
-int cmp_str(const void *a, const void *b)
+static int cmp_str(const void *a, const void *b)
 {
-    return strcmp((char *)a, (char *)b);
+    return strcmp(*(const char **)a, *(const char **)b);
 }
 
-static void create_new_entry(int pid)
+static int create_new_entry(int pid)
 {
     struct info_node *entry;
     entry = (struct info_node *)kmalloc(sizeof(struct info_node), GFP_KERNEL);
@@ -66,27 +68,40 @@ static void create_new_entry(int pid)
     entry->type = UNDEFINED;
     entry->next = NULL;
 
+    mutex_lock(&lock);
     if (head == NULL) {
         head = entry;
     } else {
         struct info_node *p = head;
-        while (p->next != NULL)
+        int flg = 1;
+        while (p->next != NULL) {
+            if (p->pid == pid) {
+                flg = 0;
+                break;
+            }
             p = p->next;
+        }
+        if (!flg || (p->pid == pid)) {
+            printk(KERN_INFO "Entry already exists for pid = %d", pid);
+            mutex_unlock(&lock);
+            return -1;
+        }
 
         p->next = entry;
     }
+    mutex_unlock(&lock);
 
     printk(KERN_INFO "Created new entry for pid = %d", pid);
+    return 0;
 }
 
-static void fill_entry(struct info_node *entry, const char *buf)
+static int fill_entry(struct info_node *entry, const char *buf)
 {
     entry->size = (int)buf[1];
 
     if (buf[0] == (char)TYPE_INT) {
         entry->type = INT;
         entry->data.intp = (int *)kmalloc(sizeof(int) * entry->size, GFP_KERNEL);
-        printk(KERN_INFO "Allocated %d size of intp", ksize(entry->data.intp));
     } else if (buf[0] == (char)TYPE_STRING) {
         entry->type = STRING;
         entry->data.charp = (char **)kmalloc(sizeof(char *) * entry->size, GFP_KERNEL);
@@ -94,49 +109,56 @@ static void fill_entry(struct info_node *entry, const char *buf)
         int i;
         for (i = 0; i < entry->size; i++)
             entry->data.charp[i] = (char *) kmalloc(sizeof(char) * MAX_STR_SIZE, GFP_KERNEL);
+    } else {
+        printk(KERN_INFO "Object type invalid!");
+        return -EINVAL;
     }
 
     printk(KERN_INFO "Filled entry for pid = %d, size = %d, type = %d", current->pid, entry->size, entry->type);
+    return 0;
 }
 
 static int open(struct inode *inodep, struct file *filep)
 {
     printk(KERN_INFO "Open called");
-    if (current == NULL) {
-        printk(KERN_INFO "current is null! unexpected!");
-        return -1;
-    }
-    create_new_entry(current->pid);
-    return 0;
+    return create_new_entry(current->pid);
 }
 
 void delete_entry(int pid)
 {
+    mutex_lock(&lock);
     struct info_node *ptr, *ptr2;
 
-    for (ptr = head; ptr != NULL; ptr = ptr->next) {
+    for (ptr = head, ptr2 = NULL; ptr != NULL; ptr = ptr->next) {
         if (ptr->pid == pid) {
-
-            /*
-               if (ptr->type == INT)
-               kfree((void *)ptr->data.intp);
-               else if (ptr->type == STRING) {
-            // free all strings
-            kfree((void *) ptr->data.charp);
+            if (ptr->type == INT)
+                kfree((void *)ptr->data.intp);
+            else if (ptr->type == STRING) {
+                // free all strings
+                int i;
+                for (i = 0; i < ptr->size; i++)
+                    kfree((void *)ptr->data.charp[i]);
+                kfree((void *) ptr->data.charp);
             }
 
+            if (ptr2 == NULL) head = ptr->next;
+            else
+                ptr2->next = ptr->next;
+
             kfree((void *)ptr);
-            */
+
             printk(KERN_INFO "Freed up entry for pid = %d" , pid);
             // Todo: multiple files per process
-            return;
+            break;
         }
+        ptr2 = ptr;
     }
+    mutex_unlock(&lock);
 }
 
 static int release(struct inode *inodep, struct file *filep){
+    delete_entry(current->pid);
     printk(KERN_INFO "File successfully closed");
-    // delete_entry(current->pid);
     return 0;
 }
 
@@ -145,47 +167,54 @@ static ssize_t write(struct file *file, const char *buff, size_t count, loff_t *
     printk(KERN_INFO "Write from process pid = %d", current->pid);
     int pid = current->pid;
 
+    mutex_lock(&lock);
     struct info_node *entry;
 
     for (entry = head; entry != NULL; entry = entry->next) {
         if (entry->pid == pid) {
+            int ret = 0;
             if (entry->type == UNDEFINED) {
                 char buf[2];
                 copy_from_user(buf, buff, 2 * sizeof(char));
 
-                fill_entry(entry, buf);
-                return entry->size;
+                ret = fill_entry(entry, buf);
+            } else if (entry->writep == entry->size){
+                ;// ignore
             } else if (entry->type == INT) {
                 // error handling
-                /*                char buffer[4];
-                                  copy_from_user(buffer, buff, 4 * sizeof(char));
-                                  printk(KERN_INFO "Got int : %s", buffer);
-                                  copy_from_user(&(entry->data.intp[entry->writep]), buff, sizeof(int));
-                                  printk(KERN_INFO "Wrote %d, Now %d", entry->data.intp[entry->writep], entry->writep + 1);
-                                  } else if (entry->type == STRING) {
-                // error handling
+                char buffer[4];
+                copy_from_user(buffer, buff, 4 * sizeof(char));
+                kstrtoint(buffer, 10, &(entry->data.intp[entry->writep]));
+
+                entry->writep++;
+                ret = entry->size - entry->writep;
+                if (entry->writep == entry->size) {
+                    printk(KERN_INFO "Sorting numbers");
+                    sort(entry->data.intp, entry->size, sizeof(int), &cmp_int, NULL);
+                }
+            } else if (entry->type == STRING) {
                 copy_from_user(entry->data.charp[entry->writep], buff, count + 1);
                 printk(KERN_INFO "charp-> %s", entry->data.charp[entry->writep]);
-                }
-                */
+
                 entry->writep++;
-            /*
-               if (entry->writep == entry->size) {
-               if (entry->type == INT)
-               sort(entry->data.intp, entry->size, sizeof(int), &cmp_int, NULL);
-               else if (entry->type == STRING)
-               sort(entry->data.charp, entry->size, sizeof(char *), &cmp_str, NULL);
-               */
-        }
-        // error handling
-        return entry->size - entry->writep;
+                ret = entry->size - entry->writep;
+                if (entry->writep == entry->size){
+                    printk(KERN_INFO "Sorting strings");
+                    sort(entry->data.charp, entry->size, sizeof(char *), &cmp_str, NULL);
+                }
+            } else {
+                printk(KERN_INFO "Invalid argument found!");
+                ret = -EINVAL;
+            }
+            mutex_unlock(&lock);
+            return ret;
         }
     }
 
-    printk(KERN_INFO "Not found pid = %d", pid);
+    mutex_unlock(&lock);
 
-    // error handling
-    return -1; // entry->size
+    printk(KERN_INFO "Not found pid = %d", pid);
+    return -EBADF;
 }
 
 static ssize_t read(struct file *file, char *buf, size_t count, loff_t *pos)
@@ -194,36 +223,42 @@ static ssize_t read(struct file *file, char *buf, size_t count, loff_t *pos)
     int pid = current->pid;
     struct info_node *entry;
 
+    mutex_lock(&lock);
     for (entry = head; entry != NULL; entry = entry->next) {
         if (entry->pid == pid) {
             // error handling
+            int ret = 0;
+            if (entry->writep < entry->size) {
+                printk(KERN_INFO "Can't read until all writes finish");
+                ret = -EACCES;
+            }
             if (entry->readp == entry->size) {
                 printk(KERN_INFO "No element left to read");
-                return -1;
+                ret = -1;
             }
-
             if (entry->type == INT) {
-                printk(KERN_INFO "Copying %d to user space buf", entry->data.intp[entry->readp]);
-                copy_to_user(buf, &(entry->data.intp[entry->readp]), sizeof(int));
+                snprintf(buf, sizeof(int), "%d", entry->data.intp[entry->readp]);
             } else if (entry->type == STRING) {
-                copy_to_user(buf, entry->data.charp[entry->readp], sizeof(char) * strlen(entry->data.charp[entry->readp]));
-            } else {
-                // error handling
+                copy_to_user(buf, entry->data.charp[entry->readp], strlen(entry->data.charp[entry->readp]) + 1);
             }
 
+            printk(KERN_INFO "COPIED buf = %s, %d, readp = %d", buf, strlen(buf), entry->readp);
             entry->readp++;
-            return 0; // return size;
+            mutex_unlock(&lock);
+            return ret;
         }
     }
-    //error handling
+    mutex_unlock(&lock);
+
+    printk(KERN_INFO "No entry found for pid = %d", pid);
     return -1;
 }
 
 
 static struct file_operations file_ops = {
     .open = open,
-    //    .write = write,
-    //    .read = read,
+    .write = write,
+    .read = read,
     .release = release,
 };
 
@@ -232,10 +267,7 @@ static int mysort_init(void)
     printk(KERN_INFO "Mysort module activated");
     struct proc_dir_entry *entry = proc_create("partb_1_15CS30002", S_IRUGO | S_IWUGO, NULL, &file_ops);
     if (entry == NULL)
-    {
-        printk(KERN_INFO "-enoent from init");
         return -ENOENT;
-    }
     return 0;
 }
 
--- LKM/1_B/test.c
+++ LKM/1_B/test.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include<sys/wait.h>
 
 #define MAX_INT 100
 
@@ -14,60 +15,175 @@ int cmp_int(const void * a, const void * b)
     return ( *(int*)a - *(int*)b );
 }
 
-int main()
+int cmp_str(const void * a, const void * b)
 {
-    int fd, value, number;
+    return strcmp((const char *)a, (const char *)b);
+}
+
+int _open(){
+    int fd;
     fd = open("/proc/partb_1_15CS30002", O_RDWR);
     if(fd < 0) {
         printf("errror opening file\n");
-        return 1;
+        return -1;
     }
-    /*
-    // Integers sorting
+    return fd;
+}
+
+int test_integers()
+{
+    int fd;
+    if ((fd = _open()) < 0) return fd;
+    // Testing Integer objects
     int N = 20;
     // write metadata
     char buf[2];
     buf[0]= (char)0xFF;
     buf[1] = (char)N;
 
-    printf("%ld\n", write(fd, buf,strlen(buf)));
+    if(write(fd, buf,strlen(buf)) < 0) {
+        printf("Write for metadata failed\n");
+        close(fd);
+        return -1;
+    }
 
-
-    int i;
+    int i, flg = 0;
     int arr[N];
+    printf("Writing numbers to LKM\n");
     for (i = 0; i < N; i++) {
-    int val = rand() % MAX_INT;
-    char buff[4]; // 4 bytes for 32 bit ints
-    sprintf(buff, "%d", val);
-    arr[i] = val;
+        int val = rand() % MAX_INT;
+        char buff[4]; // 4 bytes for 32 bit ints
+        sprintf(buff, "%d", val);
+        arr[i] = val;
 
-    printf("%s %d\n", buff, strlen(buff));
-
-    if (write(fd, buff, strlen(buff)) != (N - i - 1)) {
-    printf("Unexpected behaviour from write\n");
-    break;
+        if (write(fd, buff, strlen(buff)) != (N - i - 1)) {
+            printf("Unexpected behaviour from write\n");
+            flg = -1;
+            break;
+        }
     }
+
+    if (flg == 0) {
+        printf("Writes completed\n");
+        printf("Array of size %d generated below: \n", N);
+        for (int i = 0; i < N; i++)
+            printf("%d ", arr[i]);
+        printf("\n\nReading numbers from LKM\n");
+
+        qsort(arr, N, sizeof(int), cmp_int);
+
+        for (i = 0; i < N; i++) {
+            char buff[4];
+
+            if (read(fd, buff, 4) < 0) {
+                printf("Unexpected behaviour from read\n");
+                break;
+            };
+            int v = atoi(buff);
+            if (arr[i] != v) {
+                printf("Sorting order wrong from LKM: i = %d, expected = %d, found =  ", arr[i], v);
+                break;
+            }
+            if (i ==(N - 1))
+                printf("Sorted output matched\n");
+        }
     }
-     *
-     qsort(arr, N, sizeof(int), cmp_int);
-
-     for (i = 0; i < N; i++) {
-     char buff[4];
-
-     if (read(fd, buff, 4) < 0) {
-     printf("Unexpected behaviour from read\n");
-// break;
-};
-int v = atoi(buff);
-if (arr[i] != v) {
-printf("Sorting order wrong from LKM: i = %d, expected = %d, found =  ", arr[i], v);
-break;
+    close(fd);
+    return 0;
 }
-}
-*/
-close(fd);
 
-printf("Closing file\n");
-return 0;
+int test_strings()
+{
+    int N = 6;
+    int fd;
+
+    if ((fd = _open()) < 0) return fd;
+    // write metadata
+    char buf[2];
+    buf[0]= (char)0xF0;
+    buf[1] = (char)N;
+
+    if(write(fd, buf,strlen(buf)) < 0) {
+        printf("Write for metadata failed\n");
+        close(fd);
+        return -1;
+    }
+
+    int i, flg = 0;
+    char arr[N][100];
+    printf("Writing strings to LKM\n");
+    for (i = 0; i < N; i++) {
+        scanf("%100s", arr[i]);
+
+        if (write(fd, arr[i], strlen(arr[i])) != (N - i - 1)) {
+            printf("Unexpected behaviour from write\n");
+            flg = -1;
+            break;
+        }
+    }
+
+    if (flg == 0) {
+        printf("Writes completed\n");
+        printf("Given Array of size %d below: \n", N);
+        for (int i = 0; i < N; i++)
+            printf("%s\n", arr[i]);
+        printf("\n\nReading strings from LKM\n");
+
+        qsort(arr, N, 100, cmp_str);
+
+        for (i = 0; i < N; i++) {
+            char buff[100];
+
+            if (read(fd, buff, 100) < 0) {
+                printf("Unexpected behaviour from read\n");
+                break;
+            };
+            printf("%d %s\n", i, buff);
+            if (strcmp(buff, arr[i]) != 0) {
+                printf("Sorting order wrong from LKM:, expected = %s, found = %s\n", arr[i], buff);
+                break;
+            }
+            if (i ==(N - 1))
+                printf("Sorted output matched\n");
+        }
+    }
+
+    close(fd);
+    return 0;
+}
+
+int test_concurrency()
+{
+    int pid = fork(), ret = 0;
+    if (pid != 0) {
+        printf("Testing from Parent with pid = %d\n", getpid());
+        ret = test_integers();
+        wait(NULL);
+    } else {
+        printf("Testing from Child with pid = %d\n", getpid());
+        ret = test_integers();
+    }
+
+    return ret;
+}
+
+
+int main(int argc, char **argv)
+{
+    int val = 2;
+    if (argc > 1)
+        val = atoi(argv[1]);
+    printf("Val = %d\n", val);
+
+    if (val % 2 == 0)
+        if (test_integers() < 0)
+            printf("Integers test failed\n");
+    if (val % 3 == 0)
+        if (test_strings() < 0)
+            printf("Strings test failed\n");
+    if (val % 5 == 0)
+        if (test_concurrency() < 0)
+            printf("Concurrecy test failed\n");
+    return 0;
 }
 
